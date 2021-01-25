ef66ca4060c511f8f6a494d565f15e44b14f3cf8
raw7330 <ryosuke.bdc@gmail.com>
2021-01-25 06:06:49 +0000

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_correct_user,{only: [:edit]}
+
 
   def edit
     @user = User.find(params[:id])
   end
-
 
   def update
    @user = User.find(params[:id])
@@ -29,11 +30,20 @@ class UsersController < ApplicationController
     @books = Book.all
     @book = Book.new
   end
+ 
 
   private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 
 end
