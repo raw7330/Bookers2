0214f6635cfa23356289afca9db80b05dbf1a564
raw7330 <ryosuke.bdc@gmail.com>
2021-02-04 15:36:16 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  
   resources :books
   resources :users
   root to: 'homes#top'
@@ -9,6 +9,11 @@ Rails.application.routes.draw do
   resources :books, only: [:new, :create, :index, :show, :destroy] do
    resources :book_comments, only: [:create, :destroy]
    resource :favorites, only: [:create, :destroy]
+  
+  post 'follow/:id' => 'relationships#follow',as:'follow'
+  post 'unfollow/:id' => 'relationships#unfollow',as:'anfollow'
+
+   
   end
 
 end
