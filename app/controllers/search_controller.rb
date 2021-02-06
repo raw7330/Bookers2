9aaaf30f1f6b0eebc5f0aea74efeb51e4399485e
raw7330 <ryosuke.bdc@gmail.com>
2021-02-06 08:15:40 +0000

    finish

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,58 +1,15 @@
 class SearchController < ApplicationController
-  def search
-    @model = params["search"]["model"]
-    @content = params["search"]["content"]
-    @how = params["search"]["how"]
-    @datas = search_for(@how, @model, @content)
-  end
-  
-    private
-  def match(model, content)
-    if model == 'user'
-      User.where(name: content)
-    elsif model == 'book'
-      Book.where(title: content)
-    end
-  end
+ 	before_action :authenticate_user!
 
-  def forward(model, content)
-    if model == 'user'
-      User.where("name LIKE ?", "#{content}%")
-    elsif model == 'book'
-      Book.where("title LIKE ?", "#{content}%")
-    end
-  end
-
-  def backward(model, content)
-    if model == 'user'
-      User.where("name LIKE ?", "%#{content}")
-    elsif model == 'post'
-      Book.where("title LIKE ?", "%#{content}")
-    end
-  end
-
-  def partical(model, content)
-    if model == 'user'
-      User.where("name LIKE ?", "%#{content}%")
-    elsif model == 'post'
-      Book.where("title LIKE ?", "%#{content}%")
-    end
-  end
-
-  def search_for(how, model, content)
-    case how
-    when 'match'
-      match(model, content)
-    when 'forward'
-      forward(model, content)
-    when 'backward'
-      backward(model, content)
-    when 'partical'
-      partical(model, content)
-    end
-  end
-  
-  
+	def search
+		@model = params[:model]
+		@content = params[:content]
+		@method = params[:method]
+		if @model == 'user'
+			@records = User.search_for(@content, @method)
+		else
+			@records = Book.search_for(@content, @method)
+		end
+	end
 end
-
 
