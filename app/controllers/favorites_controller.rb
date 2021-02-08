dd2978a5dd07f7be4661f8c683c8e042c216080e
raw7330 <ryosuke.bdc@gmail.com>
2021-02-08 05:11:58 +0000

    start

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,6 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
 
@@ -13,7 +12,6 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
 
 end
